488244475739e1e3bdcbaeddd4765dd483f6be15
Thanawat080 <56470827+Thanawat080@users.noreply.github.com>
2021-10-30 15:26:13 +0700

    Add files via upload

--- screen/Login.js
+++ screen/Login.js
@@ -1,26 +1,72 @@
 import React, {useState, useContext, useEffect} from 'react';
-import { Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View,ImageBackground } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View,ImageBackground, Alert } from 'react-native';
 import * as Animatable from 'react-native-animatable'
 
 import { Context as AuthContext } from '../context/AuthContext';
 import Header from "../components/Hearders";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+
+
 const Login = ({navigation}) => {
+
     const {state, login, tryLocalSignin} = useContext(AuthContext);
     const [id_card, setId_Card] = useState("");
     const [password, setPassword] = useState("");
-
-    
     useEffect(() => {tryLocalSignin();}, []);
+   
 
 
+const [checkValidId_card , setCheckValidId_card ] = useState(true)
+const [error, setError] = useState("");
+const valid_idCard = (value) => {
+  if(value.length > 12 && value.length < 14){
+    setError(" ")
+    setCheckValidId_card(true)
+  }
+  else{
+    setError("IdCrad must be a number and have 13 characters.")
+    setCheckValidId_card(false)
+  }
+  setId_Card(value)
+}
 
+
+const [checkValidPassword , setCheckValidPassword ] = useState(true)
+const [error2, setError2] = useState("");
+const valid_password = (value) => {
+  let reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@_=;:()*"':{}[%\$%\^&\*])(?=.{8,})/;
+  if(reg.test(value) === true){
+    setError2(" ")
+    setCheckValidPassword(true)
+  }
+  else{
+    setError2("Password must have a large, small, special, and must have 8 characters.")
+    setCheckValidPassword(false)
+  }
+  setPassword(value)
+}
+
+const Check_onsubmit = () =>{
+  if(checkValidId_card == false && checkValidPassword == true){
+    Alert.alert("IdCrad requirement is invalid.")
+
+  }else if(checkValidPassword == false && checkValidId_card == true){
+    Alert.alert("Password requirement is invalid.")
+  }
+  else if(checkValidId_card == false && checkValidPassword == false){
+    Alert.alert("Password and IdCrad requirement is invalid.")
+  }
+  else{
+    login({id_card, password})
+
+  } 
+
+}
 
 
 
   return (
-
     <View style={styles.screen}>
     <ImageBackground source={require('../assets/bg.png')} style={styles.backgroundImage}>
     <SafeAreaView style={styles.container}>
@@ -29,25 +75,45 @@ const Login = ({navigation}) => {
         <Icon name='user-o' size={20} />
         <TextInput style={styles.textInput} placeholder="ID CARD" keyboardType="default" placeholderTextColor="white"
           value={id_card}
-          onChangeText={setId_Card}
+          onChangeText={(value)=>{valid_idCard(value)
+          }}
+          
         ></TextInput>
+        
       </View>
+      {checkValidId_card ? null :
+          <Animatable.View animation="fadeInLeft" duration={500}>
+           <Text style={{color:"red",left:-36}}>{error}</Text>
+          </Animatable.View>} 
 
       <View style={[styles.action, {width: '80%', marginTop: 35}]}>
         <Icon name='lock' size={30} />
         <TextInput style={styles.textInput} placeholder="Password" keyboardType="default" secureTextEntry={true} placeholderTextColor="white"
           value={password}
-          onChangeText={setPassword}
+          onChangeText={(value)=>{valid_password(value)
+          }}
+         
           ></TextInput>
-      </View>
 
-      <TouchableOpacity style={[styles.button, {backgroundColor: '#60b45c', marginTop: 35}]}  onPress={()=> login({id_card, password})}>
+      </View>
+      {checkValidPassword ? null :
+          <Animatable.View animation="fadeInLeft" duration={500} style={{flexWrap:"nowrap"}}>
+           <Text style={{color:"red",left:25, }}>{error2}</Text>
+          </Animatable.View>} 
+
+     
+
+      <TouchableOpacity style={[styles.button, {backgroundColor: '#60b45c', marginTop: 35}]}  onPress={() => Check_onsubmit()}>
         <Text style={styles.fontButton}>Log in</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={[styles.button, {backgroundColor: '#2596be', marginTop: 15}]}  onPress={()=> {navigation.navigate("RegisterScreen")}}>
+      <TouchableOpacity style={[styles.button, {backgroundColor: '#2596be', marginTop: 15}]}  onPress={() => navigation.navigate("RegisterScreen")}>
         <Text style={styles.fontButton}>Sign Up</Text>
       </TouchableOpacity>
-      {state.errorMessage ? <Text>{state.errorMessage}</Text> : null}
+      {state.errorMessage ? 
+      <Animatable.View animation="fadeInLeft" duration={500} style={{flexWrap:"nowrap"}}>
+           <Text style={{color:"red"}}>{state.errorMessage}</Text>
+          </Animatable.View>:null} 
+   
     </SafeAreaView>
     </ImageBackground>
   </View>
@@ -96,6 +162,9 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
+  valid:{
+    left:150
+  }
 });
 
 export default Login;
