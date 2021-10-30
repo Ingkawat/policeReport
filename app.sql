68e1cbbffcf61380d9f90aa47a0acf1857294808
Thanawat080 <56470827+Thanawat080@users.noreply.github.com>
2021-10-30 15:38:58 +0700

    Add files via upload

--- app.sql
+++ app.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` varchar(14) NOT NULL UNIQUE,
+  `id_card` varchar(14) NOT NULL UNIQUE,
   `f_name` varchar(250),
   `l_name` varchar(250),
   `password` varchar(250),
-  PRIMARY KEY (`id`)
+  `confirmpassword` varchar(250),
+  `phonenumber` varchar(250),
+  `email` varchar(250),
+  PRIMARY KEY (`id_card`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
