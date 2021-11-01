a66300faed1d52eadc2e278645d219177640f034
Pommy <pomboivo@hotmail.com>
2021-11-01 17:17:58 +0700

    Add files via upload

--- screen/Notifications.js
+++ screen/Notifications.js
@@ -1,12 +1,47 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import * as React from "react";
+import { ScrollView, Text, View, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Entypo,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
 
 function Notifications() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Notifications!</Text>
-      </View>
-    );
-  }
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={{ flex: 1, alignItems: "center" }}>
+          <View
+            style={{
+              backgroundColor: "#135DD8",
+              width: "90%",
+              height: 50,
+              borderRadius: 5,
+              marginTop: 10,
+              justifyContent: 'center'
+            }}
+          >
+            <View style={[styles.row, {paddingLeft: 10}]}>
+              <Entypo name="text-document" size={20} color="white" />
+              <Text style={{paddingLeft: 10, color: 'white'}}>asd</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
 
-  export default Notifications;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+});
+
+export default Notifications;
+
