f2e62ef0940653b1cbf915bd0a4549585b085609
Pommy <pomboivo@hotmail.com>
2021-11-01 18:17:43 +0700

    Add files via upload

--- screen/Status.js
+++ screen/Status.js
@@ -11,13 +11,54 @@ import {
   Image,
   ScrollView,
 } from "react-native";
+import {
+  Entypo,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  AntDesign,
+} from "@expo/vector-icons";
 
 const Status = () => {
-  return <View style={{ flex: 1, alignItems: "center", marginTop: 20 }}>
-
-  </View>;
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={{paddingTop: 10}}>
+        <View
+          style={[styles.container, { backgroundColor: "white", height: 100 }]}
+        >
+          <View style={{ width: "20%", justifyContent: 'center', height: 100, alignItems:'center' }}>
+          <Entypo name="text-document" size={50} color="black" />
+          </View>
+          <View style={{ width: "50%", justifyContent: 'center', height: 100 }}>
+            <Text>asd</Text>
+          </View>
+          <View style={{ width: "30%", justifyContent: 'center', height: 100 }}>
+            <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+              <AntDesign name="calendar" size={20} color="black" />
+              <Text> 01/12/2564</Text>
+            </View>
+            <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+              <AntDesign name="clockcircleo" size={20} color="black" />
+              <Text> 12:00น.</Text>
+            </View>
+          </View>
+        </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start", // if you want to fill rows left to right
+  },
+  item: {
+    width: "50%", // is 50% of container width
+  },
+});
 
 export default Status;
