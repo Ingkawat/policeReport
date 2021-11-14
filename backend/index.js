ccfe1601d16275999a7d3e124459022b9f665e33
Thanawat080 <56470827+Thanawat080@users.noreply.github.com>
2021-11-14 09:10:07 +0700

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('uploads'))
 
 const loginRouter = require('./routes/login')
 app.use(loginRouter.router)
@@ -13,6 +14,11 @@ app.use(accountRouter.router)
 const reportRouter = require('./routes/report')
 app.use(reportRouter.router)
 
+const report2Router = require('./routes/report2')
+app.use(report2Router.router)
+
+
 app.listen(3000, () => {
     console.log(`Example app listening at http://localhost:3000`)
+  
   })
