5b28c5b0d8974ba4542a238f30c6f282a3927c65
Pommy <pomboivo@hotmail.com>
2021-11-17 16:20:48 +0700

    Add files via upload

--- screen/approve.js
+++ screen/approve.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { View,  Text, Button, Image,Linking} from "react-native";
+import { View,  Text, Button, Image,Linking, ScrollView} from "react-native";
 import * as DocumentPicker from 'expo-document-picker';
 
 import axios from "axios";
@@ -14,9 +14,9 @@ const Approve = ({route, navigation}) => {
 const makecall = (phone) =>{
   let phonenumber = phone
   if(Platform.OS === 'android'){
-    phonenumber = 'tel:$('+phonenumber+')'
+    phonenumber = 'tel:('+phonenumber+')'
   }else{
-    phonenumber = 'tel:$('+phonenumber+')'
+    phonenumber = 'tel:('+phonenumber+')'
   }
 
   Linking.openURL(phonenumber)
@@ -29,7 +29,7 @@ const makecall = (phone) =>{
     
       if(report_type == "เอกสารหาย"){
       await axios
-          .post(`http://192.168.1.36:3000/report/pending/${report_id}/${police_id}`)
+          .post(`http://192.168.1.113:3000/report/pending/${report_id}/${police_id}`)
           .then((res) => {
             console.log(res.data);
             //
@@ -40,10 +40,10 @@ const makecall = (phone) =>{
         }
         else if (report_type == "แจ้งคนหาย"){
           await axios
-          .post(`http://192.168.1.36:3000/report/pending/${report_id}/${police_id}`)
+          .post(`http://192.168.1.113:3000/report/pending/${report_id}/${police_id}`)
           .then(async (res) => {
             await axios
-            .put(`http://192.168.1.36:3000/report/missingpeople/detail`,{
+            .put(`http://192.168.1.113:3000/report/missingpeople/detail`,{
               report_id:report_id 
             })
             .then(async(res) => {
@@ -62,7 +62,7 @@ const makecall = (phone) =>{
 
     const Approve = async (report_id) =>{
       await axios
-      .post(`http://192.168.1.36:3000/report/pending/status/success`,{
+      .post(`http://192.168.1.113:3000/report/pending/status/success`,{
         report_id:report_id
       })
       .then((res) => {
@@ -85,7 +85,7 @@ const makecall = (phone) =>{
         data.append("id", report_id)
         data.append("image", doc)
         await axios
-          .post("http://192.168.1.36:3000/users", data)
+          .post("http://192.168.1.113:3000/users", data)
           .then((res) => {
             console.log(res.data);
           })
@@ -95,45 +95,51 @@ const makecall = (phone) =>{
         console.log("Doc: " + doc.uri);
     }
     return(
-      <View>
-      
+    <View style={{backgroundColor: 'white', height:'100%'}}>
     {report_type == "เอกสารหาย" ? 
-    <View>
-          <Button title="..." onPress={() => console.log('test')}/>
-          <Button title="..." onPress={() => console.log('test')}/>
-          <Button title="..." />
-          <Button title="select file" onPress={() => pickDocument()}/>
-          <Button title="Approve!!!" onPress={() => navigation.popToTop()}/>
-          </View>
-  :
-  <View>
-    {report != null ? 
-    <View>
-        <Text>แจ้งคนหาย</Text>
-        <Text>ข้อมูลส่วนคนหาย</Text>
+    <View style={{ margin: 25}}>
+      <Button title="select file" onPress={() => pickDocument()}/>
+      <View style={{paddingBottom: 10}}/>
+      <Button title="Approve!!!" onPress={() => navigation.popToTop()}/>
+    </View>
+    :
+    <ScrollView>
+      {report != null ? 
+      <View style={{ margin: 25}}>
+        <View style={{alignItems: 'center' , paddingBottom: 20}}>
+          <Text style={{fontWeight: 'bold', fontSize: 17.5}}>แจ้งคนหาย</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 17.5, color: '#bccdd6'}}>ข้อมูลผู้หาย</Text>
+          <Image source={{uri:"http://192.168.1.113:3000/"+report[0].image_people}} style={{ width: 100, height: 100, borderRadius: 25 }}></Image>
+          <Text style={{fontWeight: 'bold', fontSize: 15}}>{report[0].missing_name}</Text>
+        </View>
+        <Text style={{fontWeight: 'bold', fontSize: 17.5, color: '#bccdd6'}}>ID Card</Text>
+        <Text style={{fontWeight: 'bold', fontSize: 15}}>{report[0].missing_idcard}</Text>
+        <Text style={{fontWeight: 'bold', fontSize: 17.5, color: '#bccdd6'}}>Details</Text>
+        <Text style={{fontWeight: 'bold', fontSize: 15}}>{report[0].missing_des}</Text>
         <View>
-           <Image source={{uri:"http://192.168.1.36:3000/"+report[0].image_people}} style={{ width: 100, height: 100 }}></Image>
-           </View>
-           <View>  
-           <Text>{report[0].missing_name}  {report[0].missing_idcard}</Text>
-             <Text>{report[0].missing_des}</Text>
-           </View>
-           <View>
-           <Text>ข้อมูลส่วนผู้แจ้ง</Text>
-           <Button title="call" onPress={()=>{makecall(report[0].phonenumber)}}></Button>
-           <Text>{user_id}</Text>
-           <Image style={{height: 100, width: 100}} source={{uri:"http://192.168.1.36:3000/"+report[0].imageuser}}></Image>
-           <Text>{report[0].f_name} {report[0].l_name}</Text>
-           <Text>{report[0].email}</Text>
-           <Text>เวลาที่แจ้งไป {report[0].date}</Text>
-           </View>
-           <Button title="เบาะแส จากผู้คน" onPress={() => navigation.navigate("Goodpeople",{id:report[0].id})}/>
-            <Button title="Approve!!!" onPress={() => {Approve(report_id),navigation.popToTop()}}/>
-     </View>
-            :<Text>loading</Text>
-    }
-
-   </View> 
+          <View style={{alignItems: 'center' , paddingBottom: 20}}>
+          <Text style={{fontWeight: 'bold', fontSize: 17.5, color: '#bccdd6'}}>ข้อมูลผู้แจ้ง</Text>
+          <Image source={{uri:"http://192.168.1.113:3000/"+report[0].imageuser}} style={{ width: 100, height: 100, borderRadius: 25 }}></Image>
+          <Text style={{fontWeight: 'bold', fontSize: 15}}>{report[0].f_name} {report[0].l_name}</Text>
+        </View>
+          <Text style={{fontWeight: 'bold', fontSize: 17.5, color: '#bccdd6'}}>ID Card</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 15}}>{user_id}</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 17.5, color: '#bccdd6'}}>Email</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 15}}>{report[0].email}</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 17.5, color: '#bccdd6'}}>เวลาที่แจ้ง</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 15}}>{report[0].date}</Text>
+        </View>
+        <View style={{paddingBottom: 10}}/>
+        <Button title="call" onPress={()=>{makecall(report[0].phonenumber)}}/>
+        <View style={{paddingBottom: 10}}/>
+        <Button title="เบาะแส จากผู้คน" onPress={() => navigation.navigate("Goodpeople",{id:report[0].id})}/>
+        <View style={{paddingBottom: 10}}/>
+        <Button title="Approve!!!" onPress={() => {Approve(report_id),navigation.popToTop()}}/>
+      </View>
+      :
+      <Text>loading</Text>
+      }
+    </ScrollView> 
     
 }
 </View>
