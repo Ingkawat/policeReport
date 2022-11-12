1c7869a266193863bb51a1149c663a504b3d6d1d
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-11-13 01:07:57 +0700

    Update AuthContext.js

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -2,121 +2,121 @@ import createDataContext from "./createDataContext";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Alert } from "react-native";
-
-
+import Constants from 'expo-constants';
 
 const authReducer = (state, action) => {
-    switch (action.type) {
-        case 'register':
-            return {...state, isSignin: true}
-        case 'add_error':
-
-            return {...state, errorMessage: action.payload};
-        case 'signin':
-            return {errorMessage: '' , username: action.payload, role: action.role};
-        case 'getReport':
-            return {...state, report: action.payload};
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
+    case "signin":
+      return {errorMessage: action.error, username: action.payload};
+    case 'register':
+      return {...state, Message: action.error}
+    default:
+      return state;
+  }
 };
 
-const clearLocal = dispatch => async () => {
-    await AsyncStorage.removeItem("username");
-    dispatch({type: 'signin', payload: null})
-}
+const clearLocal = (dispatch) => async () => {
+  await AsyncStorage.removeItem("username");
+  dispatch({ type: "signin", payload: null });
+};
 
-const tryLocalSignin = dispatch => async () => {
-    const token = await AsyncStorage.getItem("username")
-    if(token){  
-        axios.post(`http://192.168.1.37:3000/user/${token}`)
-        .then( async (res) => {
-            
-            dispatch({type: 'signin', payload: token, role: res.data.role})
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-    
-        
-    }
-    ;
-}
-const getReport = (dispatch) => {
-    return async () => {
-        axios
-        //use your ip address type in cmd ipconfig***
-        .post(`http://192.168.1.37:3000/report/${await AsyncStorage.getItem("username")}`)
-        .then( async (res) =>dispatch({ type: 'getReport', payload: res.data }))
-        .catch((err) => {
-            console.log(err)
-        });
-    }
-}
-
-
-
-const register = (dispatch) => {
-    return({id_Card, fname, lname, password,phoneNumber,email}) => {
-        axios
-        //use your ip address type in cmd ipconfig***
-        .post("http://192.168.1.37:3000/register", {
-          id_Card: id_Card,
-          f_name: fname,
-          l_name: lname,
-          password: password,
-          phoneNumber: phoneNumber,
-          email: email
-        })
-        .then( async (res) => {
-           console.log('register sucess')
-           dispatch({type: 'register'})
-           
-        })
-        .catch((err) => {
-          dispatch({ type: 'add_error', payload: 'Register Error' })
-          console.log("error")
-        });
-    }
-}
+const tryLocalSignin = (dispatch) => async () => {
+  const token = await AsyncStorage.getItem("username");
+  console.log(token)
+  if (token) {
+    axios
+      .post(`http://${Constants.expoConfig.extra.apiUrl}:3000/user/${token}`)
+      .then(async (res) => {
+        dispatch({ type: "signin", payload: token });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+};
 
 const login = (dispatch) => {
-    return ({id_card, password}) => {
-        axios
-        //use your ip address type in cmd ipconfig***
-        .post("http://192.168.1.37:3000/login", {
-          id_card: id_card,
-          password: password,
-        })
-        .then( async (res) => {
-           await AsyncStorage.setItem('username', id_card)
-           dispatch({type: 'signin', payload: id_card, role: res.data.role})
-      
-        })
-        .catch((err) => {
-
-            dispatch({ type: 'add_error', payload: 'Incorrect Username and/or Password!'})
-        
+  return ({ user_name, password }) => {
+    axios
+      .post(`http://${Constants.expoConfig.extra.apiUrl}:3000/login`, {
+        user_name: user_name,
+        password: password,
+      })
+      .then(async (res) => {
+        console.log(res.data)
+        if(res.data == "Login success"){
+        console.log("login is success")
+        await AsyncStorage.setItem("username", user_name);
+        dispatch({ type: "signin", payload: user_name });
+        }else{
+          dispatch({ type: "signin", error: res.data });
+        }
+      })
+      .catch((err) => {
+        dispatch({
+          type: "add_error",
+          payload: "Incorrect Username or Password!",
         });
+      });
+  };
+};
 
-    }
+function dataURItoBlob(dataURI) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+  else
+      byteString = unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
 }
 
-const signout = (dispatch) => {
-    return()=>{
-        //make signout
-        // axios.get("http://localhost:3000/user")
-        // .then((res) => {
-        //     console.log(res)
-        // })
-        // .catch((err) => {
-        //     console.log(err);
-        // });
+const register = (dispatch) => {
+  return({user_name, password, name, email, phone, image}) => {
+    const data = new FormData();
+    
+    if(image != undefined){
+      var blob = dataURItoBlob(image);
+      data.append("images", blob)
+    }else{
+      data.append("images", null)
     }
+    
+    data.append("user_name", user_name)
+    data.append("password", password)
+    data.append("name", name)
+    data.append("email", email)
+    data.append("phone", phone)
+    
+      axios
+      //use your ip address type in cmd ipconfig***
+      .post(`http://${Constants.expoConfig.extra.apiUrl}:3000/signup`,data, {headers:{'Content-Type': "multipart/form-data"}} 
+      )
+      .then( async (res) => {
+         dispatch({type: 'register', error: res.data})
+         
+      })
+      .catch((err) => {
+        dispatch({ type: 'add_error', payload: 'Register Error' })
+        console.log(err)
+      });
+  }
 }
 
-export const {Provider, Context} = createDataContext(
-    authReducer,
-    {login, signout, register, tryLocalSignin, clearLocal, getReport},
-    {username: null, errorMessage:'', isSignin: false, report: [], role:''}
-)
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { login, register, tryLocalSignin, clearLocal},
+  { username: null, errorMessage: '', Message:''}
+);
